01ddebefa0513596ed0c5a0cc38c971acb561c7e
Tim Gorr <TGorr01@gmail.com>
2024-09-02 18:08:45 +0200

    Week 1 Assignment

---
+++ experiments/cubicDisarray.js
@@ -0,0 +1,57 @@
+let size = 400;
+let squareSize = 30; 
+let randomDisplacement = 15; 
+let offset = 10; 
+let rotationSlider;
+
+function setup() {
+  createCanvas(size, size); 
+  rotationSlider = createSlider(0, 1, 0, 1);
+  rotationSlider.addClass('rotationSlider');
+  let sliderLabel = createP('Displacement-Switch');
+  sliderLabel.position(55,385); 
+  sliderLabel.style('font-family', 'Arial');
+  sliderLabel.style('font-size', '16px');
+  sliderLabel.style('color', '#333');
+  noLoop(); 
+}
+
+function createCubes() {
+  let rotateMultiplier = rotationSlider.value(); 
+  clear(); 
+  
+
+  for (let i = squareSize; i <= size - squareSize; i += squareSize) {
+    for (let j = squareSize; j <= size - squareSize; j += squareSize) {
+      
+      let translateAmtX = 0;
+      let translateAmtY = 0;
+      let rotateAmt = 0;
+
+      if (rotateMultiplier > 0) {
+        let plusOrMinus = random() > 0.5 ? -1 : 1;
+        rotateAmt = (j / size) * PI / 180 * plusOrMinus * random() * rotateMultiplier;
+
+        plusOrMinus = random() > 0.5 ? -1 : 1;
+        translateAmtX = (i / size) * plusOrMinus * random() * randomDisplacement;
+        translateAmtY = (j / size) * plusOrMinus * random() * randomDisplacement;
+      }
+
+      push(); 
+      translate(i + translateAmtX + offset, j + translateAmtY + offset); 
+      rotate(rotateAmt); 
+      rectMode(CENTER); 
+      strokeWeight(2);
+      rect(0, 0, squareSize, squareSize); 
+      pop(); 
+    }
+  }
+}
+
+function draw() {
+  createCubes();
+}
+
+function mouseReleased() {
+  redraw(); // Redraw when the mouse is released
+}
---
+++ experiments/weirdCircle.js
@@ -0,0 +1,52 @@
+let osc, playing, freq, amp;
+let noiseMax = 1;
+let slider;
+
+function setup() {
+    let cnv = createCanvas(800, 800);
+    slider = createSlider(0, 30, 0, 0.2);
+    let sliderLabel = createP('Chaos Range Slider (click on canvas to play sound)');
+    sliderLabel.position(137,785); 
+    sliderLabel.style('font-family', 'Arial');
+    sliderLabel.style('font-size', '16px');
+    sliderLabel.style('color', '#333');
+    cnv.mousePressed(playOscillator);
+    osc = new p5.Oscillator('sine');
+}
+
+function draw() {
+    background(0);
+    translate(width / 2, height / 2);
+    stroke(255);
+    noFill();
+    
+    noiseMax = slider.value(); 
+    
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += 0.05) {
+        let xoff = map(cos(a), -1, 1, 0, noiseMax);
+        let yoff = map(sin(a), -1, 1, 0, noiseMax);
+        let r = map(noise(xoff, yoff), 0, 1, 50, 350);
+        let x = r * cos(a);
+        let y = r * sin(a);
+        vertex(x, y);
+        
+        if (playing) {
+          
+            freq = map(r, 50, 350, 100, 1000);  
+            osc.freq(freq, 0.1); 
+            osc.amp(0.5, 0.1);    
+        }
+    }
+    endShape(CLOSE);
+}
+
+function playOscillator() {
+    osc.start();
+    playing = true;
+}
+
+function mouseReleased() {
+    osc.amp(0, 0.5);
+    playing = false;
+}
