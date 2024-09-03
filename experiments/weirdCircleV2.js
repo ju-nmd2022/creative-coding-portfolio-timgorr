1a31206c079b5c76581dafeeae27ff3b12d62f91
Tim Gorr <TGorr01@gmail.com>
2024-09-03 11:49:27 +0200

    Variation

--- experiments/weirdCircleV2.js
+++ experiments/weirdCircleV2.js
@@ -16,9 +16,9 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+    // background(0);
     translate(width / 2, height / 2);
-    stroke(255);
+    stroke(100);
 
     
     noiseMax = slider.value(); 
